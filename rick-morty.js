5d4c74dbd27eec0e0abce371cd1575d9c9e25926
ribeirosofia <ribeiro.sofia.sr@gmail.com>
2025-07-28 21:40:47 -0300

    fix: realterando a resposta

--- rick-morty.js
+++ rick-morty.js
@@ -2,6 +2,7 @@ const rickMortyURL = "https://rickandmortyapi.com/api/character";
 
 
 async function getCharacters(name){
+    const response = await fetch(`${rickMortyURL}/?name=${name}`);
     const characterData = await response.json();
     return characterData;
 }

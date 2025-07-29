96b25af5f4af8c24198a884e196d4334d6fab6f3
ribeirosofia <ribeiro.sofia.sr@gmail.com>
2025-07-28 21:33:46 -0300

    fix: alterações centrais no projeto

--- rick-morty.js
+++ rick-morty.js
@@ -1,17 +1,14 @@
 const rickMortyURL = "https://rickandmortyapi.com/api/character";
 
+
 async function getCharacters(name){
-    const response = await fetch(`${rickMortyURL}/?name=${name}`);
     const characterData = await response.json();
     return characterData;
 }
 
 async function main(){
-<<<<<<< HEAD
-    const characterData = await getCharacters('morty');
-=======
-    const characterData = await getCharacters('artisticmorty');
->>>>>>> ed04ce8 (feat: aterações no codigo)
+    const personagem = process.argv[2] || 'rick';
+    const characterData = await getCharacters(personagem);
     console.log(characterData);
 
 }
